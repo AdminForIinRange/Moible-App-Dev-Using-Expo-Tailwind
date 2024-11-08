6ccd07ab425e4660b8b8d85c582f7c5e972366d4
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-08 17:41:16 +1030

    layoutImport

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Slot} from 'react-native';
+import { View, Text } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Hova</Text>
+      <Text className="text-2xl font-500r">Testing Connection to Expo</Text>
       <StatusBar style="auto" />
     </View>
   );
