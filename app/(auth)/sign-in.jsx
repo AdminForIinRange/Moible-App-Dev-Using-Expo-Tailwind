90ef0fa019aca57d22d906ba50d4d153be885628
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-08 21:10:15 +1030

    import { useState } from "react"; import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
    import { icons } from "../constants";
    const FormField = ({
      title,
      value,
      placeholder,
      handleChangeText,
      otherStyles,
      ...props
    }) => {
      const [showPassword, setShowPassword] = useState(false);
      return (
        <View className={`space-y-2 ${otherStyles}`}>
          <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
          <View className="flex-row border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center">
            <TextInput
              className="flex-1 text-white font-pmedium text-base"
              value={value}
              placeholder={placeholder}
              placeholderTextColor={"#7b7b8b"}
              onChangeText={handleChangeText}
              secureTextEntry={title === "Password" && !showPassword}
            />
    
            {title === "Password" && (
              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
                <Image
                  source={!showPassword ? icons.eye : icons.eyeHide}
                  className="w-6 h-6"
                  resizeMode="contain"
                />
              </TouchableOpacity>
            )}
          </View>
        </View>
      );
    };
    
    export default FormField;

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,7 +3,7 @@ import { Link, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
 import { images } from "../../constants";
-import { FormField } from "../../components/FormField";
+import  FormField  from "../../components/FormField";
 
 
 const SignIn = () => {
