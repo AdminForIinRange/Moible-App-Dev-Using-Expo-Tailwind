a56529f0cdf513806c51d23b35feb6d2a71d0a32
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-09 18:47:11 +1030

    adding home page comps

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,15 +1,65 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, Image, RefreshControl, Text, View } from "react-native";
+
+import { images } from "../../constants";
+
+import  SearchInput  from "../../components/SearchInput";
+
+import EmptyState from "../../components/EmptyState";
 
 const Home = () => {
   return (
-    <View>
-      <FlatList 
-      data={[{id: 1}, {id: 2}, {id: 3}]}
-      keyExtractor={item => item.$id} 
-      renderItem={({item}) => (<Text className="text-3xl">{item.id}</Text>)}/>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <Text className="text-3xl">{item.id}</Text>}
+        ListHeaderComponent={() => (
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  JSMastery
+                </Text>
+              </View>
 
-export default Home
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+            <SearchInput />
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
+                Latest Videos
+              </Text>
+
+              {/* <Trending
+              //  posts={latestPosts ?? []}
+               /> */}
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos created yet"
+          />
+        )}
+        // refreshControl={
+        //   // <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        // }
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
