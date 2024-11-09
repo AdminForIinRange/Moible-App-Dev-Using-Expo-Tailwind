1bde4fb0807c394017ee139c220c490d19c9456d
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-09 23:00:06 +1030

    small fix

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -42,7 +42,7 @@ const SignIn = () => {
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
         <View
-          className="w-full flex justify-center h-full px-4 my-6"
+          className="w-full flex   px-4 my-6"
           style={{
             minHeight: Dimensions.get("window").height - 100,
           }}
